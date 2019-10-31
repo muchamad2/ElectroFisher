e03584a93812d32074633f6da3998f24023b3c25
muchamad2 <muhamadirsad146@gmail.com>
2019-10-31 12:49:40 +0700

    Add summary documentary of data xml

--- Assets/Scripts/System/GameUtility.cs
+++ Assets/Scripts/System/GameUtility.cs
@@ -2,47 +2,71 @@ using UnityEngine;
 using System.IO;
 using System.Xml.Serialization;
 
-public enum EnvironmentType {Forest,Sea}
-public enum Language{Indo,Eng}
-public static class GameUtility{
+public enum EnvironmentType { Forest, Sea }
+public enum Language { Indo, Eng }
+public static class GameUtility
+{
     public static int PlayerScore { get; set; }
     public static int PlayerHealth { get; set; }
-    
+
     public static EnvironmentType environmentType { get; set; }
 
     public static Language LangType { get; set; }
-    public const string  xmlFileName = "Question_Data.xml";
-    public static string xmlFilePath{
-        get{
-            return Application.dataPath+ "/Resources/"+((LangType == Language.Indo) ? "id_":"eng_")+xmlFileName;
+    public const string xmlFileName = "Question_Data.xml";
+    public static string xmlFilePath
+    {
+        get
+        {
+            return Application.dataPath + "/Resources/" + ((LangType == Language.Indo) ? "id_" : "eng_") + xmlFileName;
         }
     }
 }
+
+/// <summary>
+/// mendapatkan data dari xml file kedalam kelas data dan meyimpan data ke dalam bentuk xmlfile,
+/// fungsi **Write(Data data)** untuk menyimpan data kedalam xml file
+/// 
+/// fungsi **Fetch(out bool result)** mendapatkan data dari xml file dan diperlihatkan hasil dalam bentuk bool
+/// result
+/// 
+/// #Example Fetch
+/// <code>
+/// var d = Data.Fetch(out bool result);
+/// if(result){
+///    data = d;
+/// }
+/// </code>
+/// </summary>
 [System.Serializable]
-public class Data{
+public class Data
+{
 
     public Language lang = Language.Indo;
     public Question[] Questions = new Question[0];
-    public Data(){}
+    public Data() { }
 
-    public static void Write(Data data){
+    public static void Write(Data data)
+    {
         XmlSerializer serializer = new XmlSerializer(typeof(Data));
-        using (Stream stream = new FileStream(GameUtility.xmlFilePath, FileMode.Create)){
+        using (Stream stream = new FileStream(GameUtility.xmlFilePath, FileMode.Create))
+        {
             serializer.Serialize(stream, data);
         }
     }
-    public static Data Fetch(){
+    public static Data Fetch()
+    {
         return Fetch(out bool result);
     }
     public static Data Fetch(out bool result)
     {
-        if(!File.Exists(GameUtility.xmlFilePath)){result = false; return new Data();}
+        if (!File.Exists(GameUtility.xmlFilePath)) { result = false; return new Data(); }
         XmlSerializer deserializer = new XmlSerializer(typeof(Data));
-        using (Stream stream = new FileStream(GameUtility.xmlFilePath,FileMode.Open)){
+        using (Stream stream = new FileStream(GameUtility.xmlFilePath, FileMode.Open))
+        {
             var data = (Data)deserializer.Deserialize(stream);
-            
+
             result = true;
-            
+
             return data;
         }
     }
